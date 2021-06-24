570bf8fd27f045f31202dd41ba9eb9a6357c3183
Diogo Raposo <diogo.mm.raposo@gmail.com>
2021-06-24 21:04:28 +0100

    fixed flag en

--- js/brisa.js
+++ js/brisa.js
@@ -1,8 +1,8 @@
 function myLangSelect() {
     var x = document.getElementById("lang-select");
     if (x.style.display === "none") {
-      x.style.display = "inline";
+        x.style.display = "inline";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
+}
