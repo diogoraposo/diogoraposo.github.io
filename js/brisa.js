83d70653207958300d1e7c3e347aec5cf622ec01
Diogo Raposo <diogo.mm.raposo@gmail.com>
2021-06-24 19:45:53 +0100

    translation

---
+++ js/brisa.js
@@ -0,0 +1,8 @@
+function myLangSelect() {
+    var x = document.getElementById("lang-select");
+    if (x.style.display === "none") {
+      x.style.display = "inline";
+    } else {
+      x.style.display = "none";
+    }
+  }
