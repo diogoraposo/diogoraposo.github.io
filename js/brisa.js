c0b26656daadde0c9e2f3c51b9afc61c9a03242b
Diogo Raposo <diogoraposo@Diogos-MacBook-Pro.local>
2024-05-01 16:54:24 +0100

    adding space to scroll in devices where view width smaller than height

--- js/brisa.js
+++ js/brisa.js
@@ -1,8 +1,21 @@
-function myLangSelect() {
+/**function myLangSelect() {
     var x = document.getElementById("lang-select");
     if (x.style.display === "none") {
         x.style.display = "inline";
     } else {
         x.style.display = "none";
     }
-}
+}*/
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the viewport width and height
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    // Check if viewport height is greater than viewport width
+    if (viewportHeight > viewportWidth) {
+        const ulElement = document.createElement('ul');
+        const footerList = document.getElementById('footer-list');
+        footerList.appendChild(ulElement);
+        ulElement.classList.add('empty-size');
+    }
+});
